16d596964e04d69cf3a2564fdd00c5ea6c40912a
Kevin Cortina <kevinc.php@gmail.com>
2022-03-16 20:00:08 -0700

    fixed spacing, added summary text, tried something different with navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,35 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Navbar, Nav } from 'react-bootstrap';
 
 export default function Navbars ( { currentPage, handlePageChange }) {
 
   return (  
-    <nav>
-      
-      <div className="nav-section">
-
-          <ul className="mb-2">
-          <Button variant="light" size="lg" id="AboutMe"  active
+    
+      <Container className="nav-section">
+        <Nav className="me-auto">
+          <Navbar className="mb-2">
+          <Button variant="light" size="lg" id="basic-navbar-nav"  active
           onClick={({}) => handlePageChange('AboutMe')} className={currentPage === 'AboutMe' ? 'currentPage' : ''}
           > About me</Button>
-          </ul>
+          </Navbar>
 
-          <ul className="mb-2">
-          <Button variant="light" size="lg" id="ProjectMe"  active
+          <Navbar className="mb-2">
+          <Button variant="light" size="lg" id="basic-navbar-nav"  active
           onClick={() => handlePageChange('Projects')}  className={currentPage === 'Projects' ? 'currentPage' : ''}
           > Portfolio</Button>
-          </ul>
+          </Navbar>
 
-          <ul className="mb-2">
-          <Button variant="light" size="lg" id="ResumeMe" active
+          <Navbar className="mb-2">
+          <Button variant="light" size="lg" id="basic-navbar-nav" active
           onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume'? 'currentPage' : ''}
           > Resume</Button>
-          </ul>
+          </Navbar>
 
-          <ul className="mb-2">
-          <Button variant="light" size="lg" id="ContactMe" active
+          <Navbar className="mb-2">
+          <Button variant="light" size="lg" id="basic-navbar-nav" active
           onClick={( {} ) => handlePageChange('Contacts')} className={currentPage === 'Contacts'? 'currentPage' : ''}
           > Contact me</Button>
-          </ul>
-
-    </div>
-</nav>
+          </Navbar>
+        </Nav>
+    </Container>
   )};
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,14 @@ import Resume2 from '../images/Resume.jpg'
 function Resume ( handlePageChange) {
   return (  
     <div>
+
     <section id="project-1">
+      <h4 id="project-header">SUMMARY</h4>
+        <div id="description">
+        <ul>Junior Software Developer with demonstrated project history of working in the software development industry. 
+        Skilled in jQuery, Javascript, MongoDB, HTML/CSS, and MySql. 
+        Strong software developer professional graduated with a certificate from UW Professional & Continuing Education.</ul>
+        </div>
       <h4 id="project-header">WORK EXPERIENCE</h4>
         <div id="description">
         <ul>File Manager - Harrigan, Leyh, Farmer, & Thomsen </ul>
@@ -23,13 +30,13 @@ function Resume ( handlePageChange) {
           Express.js, Heroku, GitHub, JSON, HTTP( Request methods). </ul>
         <ul>Soft Skills:  Customer Service, Organization, Communication, </ul> 
         </div>
-      
-
+          
+    {/* Resume link */}
     <a id="portfolio-navigation" href={Resume} onClick={() => handlePageChange()}> 
     </a>
     <ul id="resume"> <a href={Resume2}> <b>📄 Resume</b> </a> 
     </ul>
-    
+
     </section>
     </div>
   
